431616cd55b30742a1ceb7307caa644bd7f126fc
= <=>
2022-09-06 00:21:51 -0300

    Capítulo 3

---
+++ array.py
@@ -0,0 +1,3 @@
+import array as arr
+
+print(arr.array('d', [1, 3.5]))
---
+++ heranca/Conta.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta, abstractmethod
+
+class Conta:
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
+
+    def __str__(self):
+        return "[>>Codigo {} Saldo {}<<".format(self._codigo, self._saldo)
